b4a91dfd518d7ba76c57824caf3cbfa23bd85fbb
Room4Renzo <lorenzo.afw@gmail.com>
2023-02-07 10:45:38 +0000

    created a function that makes placeholder cards for weather information. Next is to match them to data from subsequent ajax API calls

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const APIKey = `436b24e0d7bd611aac594cd2b661224e`;
-
 const searchValue = $(`#search-input`);
 const searchBtn = $(`#search-button`);
 const limit = `&limit=1`;
 const history = $(`#history`);
-// Hardcode Limit? To what?
+const today = $(`today`);
+const forecast = $(`forecast`)
 let buttons = $(`.btn`);
 
 
@@ -14,33 +14,131 @@ let locationName = $(userSearch[i]);
 let searchBtnCounter = 0;
 
 
-
-
-
-
-
-
-
 searchBtn.on(`click`, function (event) {
     event.preventDefault();
-    let searchTerm = searchValue.val();
-
-    let queryURL = (`http://api.openweathermap.org/geo/1.0/direct?q=` + searchTerm + limit + `&appid=` + APIKey);
-
+    searchWeather(searchValue.val());
+});
+function searchWeather(cityName) {
+    let geoURL = (`https://api.openweathermap.org/geo/1.0/direct?q=` + cityName + limit + `&appid=` + APIKey);
 
     $.ajax({
-        url: queryURL,
+        url: geoURL,
         method: `GET`
     }).then(function (response) {
-        let locationData = (JSON.stringify(response));
-        localStorage.setItem(`locationData`, locationData)
-        searchBtnCounter++;
 
-        userSearch.push({
-            city: searchValue.val(),
-            lat: response[0].lat,
-            lon: response[0].lon
-        })
+        let lat = response[0].lat;
+        let lon = response[0].lon;
+
+
+        let forecastURL = (`https://api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon + `&appid=` + APIKey);
+
+        $.ajax({
+            url: geoURL,
+            method: `GET`
+        }).then(function (response) {
+            console.log(response);
+            userSearch.push(cityName);
+            let locationName = $(`<button>`);
+
+            locationName.attr(`class`, `btn`);
+            locationName.text(cityName);
+            locationName.on(`click`, function (event) {
+                console.log(event.target.textContent);
+                let historyGeoURL = (`https://api.openweathermap.org/geo/1.0/direct?q=` + (event.target.textContent) + limit + `&appid=` + APIKey);
+
+                $.ajax({
+                    url: historyGeoURL,
+                    method: `GET`
+                }).then(function (response) {
+
+                    let lat = response[0].lat;
+                    let lon = response[0].lon;
+
+                    let historyForecastURL = (`https://api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon + `&appid=` + APIKey);
+
+                    $.ajax({
+                        url:historyForecastURL,
+                        method: `GET`
+                    }).then(function(response){
+                        console.log(response)
+                        dailyWeather(response);
+                    });
+                });
+            });
+
+            history.prepend(locationName);
+        });
+
+    });
+};
+
+
+// function to create cards and match callback data to variables
+function dailyWeather (response) {
+    let dailyCard = $(`<div>`);
+    let mainCard = $(`<div>`)
+    let title = $(`<h1>`)
+    let temp = $(`<p>`);
+    let wind = $(`<p>`);
+    let humidity = $(`<p>`);
+    let date = $(`<p>`);
+    let icon = $(`<img>`)
+
+    today.append(mainCard)
+    mainCard.prepend(title)
+    title.append(temp);
+    title.append(wind);
+    title.append(humidity);
+
+    forecast.append(dailyCard)
+
+
+
+    mainCard.attr({id: `mainCard`})
+    dailyCard.attr({"class": `card`});
+
+
+}
+
+// check jquery docs for whether you can add more parameters in .text() method
+// append daILY TO TODAY
+// Append 5 day to forecast
+
+
+// searchBtn.on(`click`, function (event) {
+// event.preventDefault();
+// let searchTerm = searchValue.val();
+
+// let geoURL = (`https://api.openweathermap.org/geo/1.0/direct?q=` + searchTerm + limit + `&appid=` + APIKey);
+
+
+// $.ajax({
+//     url: geoURL,
+//     method: `GET`
+// }).then(function (response) {
+//     let locationData = (JSON.stringify(response));
+//     localStorage.setItem(`locationData`, locationData)
+//     searchBtnCounter++;
+//     console.log(response);
+//     console.log(response[0].lat);
+//     console.log(response[0].lon);
+//     let lat = response[0].lat;
+//     let lon = response[0].lon;
+
+
+//     let forecastURL = (`https://api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon + `&appid=` + APIKey);
+
+//     $.ajax({
+//         url: forecastURL,
+//         method: `GET`
+//     }).then(function (response) {
+        // console.log(response);
+
+
+
+
+
+
 
         // if (searchBtnCounter !== 0) {
         //     $(`#placeholderWeather`).empty();
@@ -48,49 +146,48 @@ searchBtn.on(`click`, function (event) {
 
         // push user input into usersearch array
         //TODO: search push function to push all searched cities to the end of the userSearch array
-        // TODO: Find out how to push 2d arrays 
+        // TODO: Find out how to push 2d arrays
 
         // Creates button and changes button text
-        locationName = $(`<button>`);
-        locationName.attr(`class`, `btn`);
-        locationName.attr(`name`, response[0].name);
-        locationName.attr(`lat`, response[0].lat);
-        locationName.attr(`lon`, response[0].lon);
-        locationName.text(response[0].name + `, ` + response[0].country);
-        history.prepend(locationName);
+        // locationName = $(`<button>`);
+        // locationName.attr(`class`, `btn`);
+        // locationName.attr(`name`, response[0].name);
+        // locationName.attr(`lat`, response[0].lat);
+        // locationName.attr(`lon`, response[0].lon);
+        // locationName.text(response[0].name + `, ` + response[0].country);
+        // locationName.on(`click`, function (event) {
+        //     console.log(event);
+        //     console.log(event.target);
+        // })
+        // history.prepend(locationName);
 
-    });
-});
-userSearch.forEach(function (singleSearch) {
-    console.log(singleSearch);
+//     });
+// });
+    // userSearch.forEach(function (singleSearch) {
+    //     console.log(singleSearch);
 
-})
+    // })
 
-localStorage.getItem(`locationData`)
-let locationData = (JSON.parse(`locationData`));
-console.log(locationData)
+    // localStorage.getItem(`locationData`)
+    // let locationData = (JSON.parse(`locationData`));
+    // console.log(locationData)
 
-function showWeather(event) {
-    if (event.target.className === `btn`) {
-        console.log(`It worked!`)
-    }
-}
+    // function showWeather(event) {
+    //     if (event.target.className === `btn`) {
+    //         console.log(`It worked!`)
+    //     }
+    // }
 
-buttons.on(`click`, function (event) {
-    event.preventDefault();
-
-    let queryURL = (`api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon * `&appid=` + APIKey);
-
-    $.ajax({
-        url: queryURL,
-        method: `GET`
-    }).then(function (response) {
-        console.log(response)
-        showWeather();
-
-    });
-})
+    // buttons.on(`click`, function (event) {
+    //     event.preventDefault();
 
 
-// accesses longitude and latitude for each created button
+    //     console.log(response)
+    //     showWeather();
 
+    // });
+
+
+
+    // accesses longitude and latitude for each created button
+// })
